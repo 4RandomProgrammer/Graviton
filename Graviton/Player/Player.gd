737fe786667dc90e70f3536d28880a18084bfdf5
ARandomProgrammer <50036343+4RandomProgrammer@users.noreply.github.com>
2020-06-08 11:57:04 -0300

    aaaaaaaaaaaaaaaaaa

--- Graviton/Player/Player.gd
+++ Graviton/Player/Player.gd
@@ -1,17 +1,42 @@
 extends KinematicBody2D
 
-const GRAVITY = 0
+const GRAVITY = 10
 const SPEED = 100
 
 var movement = Vector2.ZERO
 var gravityOrder = 1
+var on_floor = false
+var UP = Vector2.UP
+var DOWN = Vector2(0,1)
+onready var Esprite = $Sprite
 
 func _physics_process(delta):
 	movement.y += gravityOrder * GRAVITY
 	
+	
 	if Input.is_action_pressed("ui_right"):
+		Esprite.flip_h = false
+		Esprite.play("Walking")
 		movement.x  += SPEED
+		movement.x = min(SPEED,movement.x)
 	elif Input.is_action_pressed("ui_left"):
-		movement.x += SPEED
+		Esprite.flip_h = true
+		Esprite.play("Walking")
+		movement.x -= SPEED
+		movement.x = max(movement.x,-SPEED)
 		
-	move_and_slide(movement)
+	else:
+		Esprite.play("Idle")
+		movement.x = 0
+		
+	if Input.is_action_just_pressed("ui_focus_next"):
+		gravityOrder *= -1
+		if gravityOrder == 1:
+			Esprite.flip_v = false
+		else:
+			Esprite.flip_v = true
+	
+	if gravityOrder == 1:
+		movement = move_and_slide(movement,UP)
+	else:
+		movement = move_and_slide(movement,DOWN)
