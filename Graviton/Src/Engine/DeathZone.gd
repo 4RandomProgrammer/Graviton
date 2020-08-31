9a003f91c09c084c5504c265208033fd6dfa23b0
ARandomProgrammer <50036343+4RandomProgrammer@users.noreply.github.com>
2020-08-31 13:50:27 -0300

    NewLv

--- Graviton/Src/Engine/DeathZone.gd
+++ Graviton/Src/Engine/DeathZone.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+export var area = "res://Areas/World.tscn"
 
 func _on_DeathZone_body_entered(_body):
-	get_tree().change_scene("res://Areas/World.tscn")
+	get_tree().change_scene(area)
--- Graviton/Src/SpecialThing/ResetJump.gd
+++ Graviton/Src/SpecialThing/ResetJump.gd
@@ -3,4 +3,12 @@ extends Area2D
 func _on_ResetJump_body_entered(body):
 	if body.has_method("set_invert_counter"):
 		body.set_invert_counter(0)
-		queue_free()
+		$Sprite.visible = false
+		$CollisionShape2D.set_deferred("disabled",true)
+		$Timer.start(-1)
+
+
+
+func _on_Timer_timeout():
+	$Sprite.visible = true
+	$CollisionShape2D.set_deferred("disabled",false)
