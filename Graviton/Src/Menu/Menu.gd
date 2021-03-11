3cc26b142db2b0b91a224db51bbb29bf1812ce06
ARandomProgrammer <50036343+4RandomProgrammer@users.noreply.github.com>
2021-03-11 10:02:33 -0300

    new fase e sprites

--- Graviton/Src/Menu/Menu.gd
+++ Graviton/Src/Menu/Menu.gd
@@ -10,19 +10,34 @@ func _on_Play_pressed():
 
 func _on_Quit_pressed():
 	select()
-	pass # Replace with function body.
+	
 
+func _on_Creditos_pressed():
+	$Creditos2.visible = true
+	select()
+
+func _on_Voltar_pressed():
+	$Creditos2.visible = false
+	select()
 
 func _on_Play_mouse_entered():
 	hover()
 
-
 func _on_Quit_mouse_entered():
 	hover()
 
+func _on_Voltar_mouse_entered():
+	hover()
+
+
+func _on_Creditos_mouse_entered():
+	hover()
 
 func hover():
 	$MenuHover.play()
 
 func select():
 	$Selected.play()
+
+
+
--- Graviton/Src/Player/Player.gd
+++ Graviton/Src/Player/Player.gd
@@ -6,7 +6,7 @@ enum {
 }
 
 const MAXGRAVITY = 245
-const SPEED = 115
+const SPEED = 125
 const MAX_JUMP_HEIGHT = 36
 const MIN_JUMP_HEIGHT = 20
 const jump_duration = 0.21
